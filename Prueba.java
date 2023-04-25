0e8eb7d53af5164f204b458da9ecc0b33e0afbc8
Victor01val <vvalolm1305@g.educaand.es>
2023-04-25 19:58:22 +0200

    prueba

--- Prueba.java
+++ Prueba.java
@@ -5,6 +5,6 @@
  */
 public class Prueba {
     public static void main(String args[]){ 
-        System.out.print("Hola Mund hola que talones");        
+        System.out.print("Holfa Mund hola que talones");        
     }
 } 
